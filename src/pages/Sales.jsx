d38d669a793cf51c9a7fd56e2e6da9d228b7da35
Aortegaaa <carl.adrian.ortega@gmail.com>
2024-07-09 16:30:15 -0400

    Add Sales Order page

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,30 +1,15 @@
 import PageNav from "../components/PageNav";
-import styles from "./Sales.module.css";
+import OrderList from '../components/OrderList';
+import styles from './Sales.module.css';
 
 export default function Sale() {
   return (
-    <main className={styles.sale}>
+    <div>
       <PageNav />
-      <section>
-        <img
-          src="img-1.jpg"
-          alt="person with dog overlooking mountain with sunset"
-        />
-        <div>
-          <h2>Sales</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est
-            dicta illum vero culpa cum quaerat architecto sapiente eius non
-            soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga
-            perspiciatis?
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
-            doloribus libero sunt expedita ratione iusto, magni, id sapiente
-            sequi officiis et.
-          </p>
-        </div>
-      </section>
-    </main>
+      <div className={styles['sales-order-container']}>
+        <h1>Your Sales Orders</h1>
+        <OrderList />
+      </div>
+    </div>
   );
 }
