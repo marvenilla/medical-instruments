a7e3f4568b126f006d17a60ee2ab8506abdc4d12
salome558 <salomeabarca89@gmail.com>
2024-07-19 23:34:33 -0400

    add salesForm in app page

---
+++ src/components/layout/input/Input.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import './Input.style.css';
+
+export const Input = ({
+  label,
+  placeholder,
+  value,
+  onChange,
+  type = 'text',
+  name = '',
+  disabled = false,
+}) => {
+  return (
+    <div className="custom-input my-3">
+      <label className="form-label custom-label">{label}</label>
+      <input
+        disabled={disabled}
+        type={type}
+        name={name}
+        className="form-control form-control-lg"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
