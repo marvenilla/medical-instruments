0493980e3b43d2be9b43727ebcc3dfcc48266cf0
Aortegaaa <carl.adrian.ortega@gmail.com>
2024-07-23 15:29:31 -0400

    Add product inventory page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
+import ProductInventory from "./pages/ProductInventory";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/home" element={<Homepage />} />
         <Route path="/sales" element={<Sales />} />
         <Route path="/shipping" element={<Shipping />} />
+        <Route path="/inventory" element={<ProductInventory />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/app" element={<AppLayout />} />
