bef65168bf566214c9135bc2b77be46208aa6e0d
marvenilla <marvenilla.mindanao@gmail.com>
2024-08-21 20:46:49 -0400

    Refactor supabase file

--- src/supabase.js
+++ src/supabase.js
@@ -2,4 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key must be provided.");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
