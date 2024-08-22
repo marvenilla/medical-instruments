3116ac10101d40ce82dcc4586cf8f99465b6962b
marvenilla <marvenilla.mindanao@gmail.com>
2024-08-21 20:06:47 -0400

    Add env file

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://srkouriqbasmxhftuqxr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNya291cmlxYmFzbXhoZnR1cXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyMTg3NTEsImV4cCI6MjAzNjc5NDc1MX0.ixidrK-EEkDPOXa8ew1_nyAffiAmvxSITviBacLub7E'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
